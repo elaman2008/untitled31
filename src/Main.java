64b6bbb2fdc880a01f0715dfec4a7d0b79f0bc6d
user <abzalovelaman600@gmail.com>
2024-10-16 15:24:26 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        method(1,2,3,4,5);
+
+        }
+    public static void method(int ... numbers){
+        for (int i:numbers){
+            System.out.println (i);
+            for (int i= 0; i<numbers.length [i++];
+
+//method(2,4,7,8,56);
+//    }
+//    public static void method (int ...numbers){
+//        int jupCount=0;
+//        int takСounter=0;
+//         for(int c: numbers){
+//             if (c % 2 == 0) {
+//                 jupCount++;
+//             }else {
+//
+//                 System.out.println("jupCount ="+ jupCount);
+//                    System.out.println("takСounter="+ takСounter);
+    }
+
+
+
+             }
+
+        }
+    }
+}
